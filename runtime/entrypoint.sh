9a681d265ce0208c1b7a46f8d93e6ff68f5e1899
Felix Klauke <felix@klauke-enterprises.com>
2022-07-23 12:02:22 +0200

    Remove debug outputs

--- runtime/entrypoint.sh
+++ runtime/entrypoint.sh
@@ -4,10 +4,6 @@ echo "Compiling plantuml diagrams..."
 
 inputFiles=$INPUT_FILES
 
-env
-
-echo "Imput Files: $inputFiles"
-
 java -jar /opt/plantuml/plantuml.jar -charset UTF-8 $inputFiles
 
 echo "Done."
